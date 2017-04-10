4a7d99f8b2ba0ef4a1db7f9ae4cbc21b48e00499
Rory McElearney <rorymcelearney@gmail.com>
2017-04-10 19:07:04 +0300

    complete solution for JWT workshop

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const fs = require('fs');
 const querystring = require('querystring');
 const request = require('request');
+const jwt = require('jsonwebtoken');
+const hapiAuthJwt2 = require('hapi-auth-jwt2');
 require('env2')('./config.env');
 
 const server = new hapi.Server();
@@ -16,7 +18,28 @@ server.connection({
   }
 });
 
-server.register(inert, (err) => {
+server.register([inert, hapiAuthJwt2], (err) => {
+  if (err) {
+    console.log(err);
+  }
+  // insert your github username here
+  const userName = 'mcelearr';
+
+  const validate = (token, validateRequest, callback) => {
+    // decoded token, it automaitcally decodes it
+    if (!token.user.username === userName) {
+      return callback(null, false);
+    }
+    return callback(null, true);
+  };
+
+  server.auth.strategy('jwt', 'jwt',
+    {
+      key: process.env.SECRET,
+      validateFunc: validate,
+      verifyOptions: { algorithms: ['HS256'] }
+    });
+
   server.route([
     {
       method: 'GET',
@@ -31,7 +54,7 @@ server.register(inert, (err) => {
     {
       method: 'GET',
       path: '/login',
-      handler: (request, reply) => {
+      handler: (loginRequest, reply) => {
         const queries = querystring.stringify({
           client_id: process.env.CLIENT_ID,
           redirect_uri: 'https://localhost:4000/welcome'
@@ -59,11 +82,55 @@ server.register(inert, (err) => {
             return reply(error);
           }
           if (!body.access_token) {
-            reply('something went wrong.')
+            return reply('something went wrong.');
           }
-          // store access token
-          reply.redirect('/');
-        })
+          const headers = {
+            'User-Agent': 'oauth_github_jwt',
+            Authorization: `token ${body.access_token}`
+          };
+          const url = 'https://api.github.com/user';
+
+          return request.get({
+            url,
+            headers
+          }, (githubPostErr, githubPostres, githubPostbody) => {
+            if (githubPostErr) {
+              console.log(githubPostErr);
+            }
+            const parsedBody = JSON.parse(githubPostbody);
+            const payload = {
+              user: {
+                username: parsedBody.login,
+                img_url: parsedBody.avatar_url,
+                user_id: parsedBody.id
+              },
+              accessToken: parsedBody.access_token
+            };
+            const JWToptions = {
+              expiresIn: Date.now() + (24 * 60 * 60 * 1000),
+              subject: 'github-data'
+            };
+            const secret = process.env.SECRET;
+            jwt.sign(payload, secret, JWToptions, (JWTerr, token) => {
+              reply
+              .redirect('/secure') // make a new route for the redirect, config it with an authentication strategy
+              .state('token', token,
+                {
+                  path: '/',  // the token is valid for every path starting with /
+                  isHttpOnly: false,
+                  isSecure: process.env.NODE_ENV === 'PRODUCTION'
+                });
+            });
+          });
+        });
+      }
+    },
+    {
+      method: 'GET',
+      path: '/secure',
+      config: { auth: 'jwt' },
+      handler: (req, reply) => {
+        reply('It worked');
       }
     }
   ]);
